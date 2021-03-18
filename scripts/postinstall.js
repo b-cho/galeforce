ab87b268afa97ddb57e8f47d07cf98eee0df40ab
bcho04 <brandon.cho@gmail.com>
2021-03-18 11:48:59 -0700

    Update dependencies

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,7 +8,12 @@ if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
 }
 
+/* The Game Client API requires all requests to be signed with a root certificate,
+which needs to be downloaded from the Riot Games servers during the post-installation step.
+Theoretically, one could set `rejectUnauthorized=false` within the axios https.Agent config,
+but this is unsafe. */
+
 const file = fs.createWriteStream(path.join(dir, 'riotgames.pem')); // downloading game client SSL certificate
 https.get('https://static.developer.riotgames.com/docs/lol/riotgames.pem', (response) => {
-    response.pipe(file);
+    response.pipe(file); // pipe output to resource/riotgames.pem.
 });
