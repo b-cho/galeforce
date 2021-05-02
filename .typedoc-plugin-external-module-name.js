8b0e8542d868b688d47698a11c1a4f2a3eb778eb
bcho04 <brandon.cho@gmail.com>
2021-05-01 20:26:54 -0700

    Update dependencies to latest versions

--- .typedoc-plugin-external-module-name.js
+++ .typedoc-plugin-external-module-name.js
@@ -2,7 +2,7 @@ module.exports = function customMappingFunction(explicit, implicit, path, reflec
     const module = implicit.split('/')[0];
     const package = implicit.split('/')[1];
     // build the module name
-    if(module === 'riot-api') return 'internals/riot-api';
+    if(module === 'riot-api') return 'internal/riot-api';
     if(module === 'root') return 'Galeforce';
     else return `internal/${package}`;
 }
